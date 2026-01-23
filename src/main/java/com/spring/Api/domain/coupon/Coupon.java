e3a0fbc26ca95f5e448a716b9aa4785672b9408e
victorguimaraesdev <victorguimaraesmax@gmail.com>
2026-01-23 09:33:02 -0300

    Init API Spring para eventos

---
+++ src/main/java/com/spring/Api/domain/coupon/Coupon.java
@@ -0,0 +1,38 @@
+package com.spring.Api.domain.coupon;
+
+import java.sql.Date;
+import java.util.UUID;
+
+import org.hibernate.annotations.ManyToAny;
+
+import com.spring.Api.domain.event.Event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "coupon")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToAny
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
