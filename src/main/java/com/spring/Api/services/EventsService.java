1f05d9318c5892d22b6f49519e8e98f7ae642fce
victorguimaraesdev <victorguimaraesmax@gmail.com>
2026-02-02 07:27:10 -0300

    feat: Service Events

--- src/main/java/com/spring/Api/services/EventsService.java
+++ src/main/java/com/spring/Api/services/EventsService.java
@@ -7,12 +7,12 @@ import java.sql.Date;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.spring.Api.domain.event.EventRequestDTO;
+import com.spring.Api.domain.event.EventsRequestDTO;
 import com.spring.Api.domain.event.Events;
 
 @Service
 public class EventsService {
-    public Events createEvent(EventRequestDTO data) {
+    public Events createEvent(EventsRequestDTO data) {
 
        String imgURL = null;
 
@@ -20,11 +20,14 @@ public class EventsService {
         imgURL = this.uploadImg((data.image()));
        } 
 
-       Events newEvents = new Events();
-       newEvents.setTitle(data.title());
-       newEvents.setDescription(data.description());
-       newEvents.setEventUrl(data.eventURL());
-       newEvents.setDate(new Date(data.date()));
+       Events newEvent = new Events();
+       newEvent.setTitle(data.title());
+       newEvent.setDescription(data.description());
+       newEvent.setEventUrl(data.eventURL());
+       newEvent.setDate(new Date(data.date()));
+       newEvent.setImgUrl(imgURL);
+
+       return newEvent;
     }
     private String uploadImg(MultipartFile multipartFile){
         return "";
