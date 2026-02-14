3275f53a70daeefce9fb72455fc38224aa574ca8
victorguimaraesdev <victorguimaraesmax@gmail.com>
2026-02-14 14:31:37 -0300

    Fix: Event model

--- src/main/java/com/spring/Api/domain/event/EventsRequestDTO.java
+++ src/main/java/com/spring/Api/domain/event/EventsRequestDTO.java
@@ -1,8 +1,5 @@
 package com.spring.Api.domain.event;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public record EventsRequestDTO(String title, String description, Long date, String state, Boolean remote, String eventURL, MultipartFile image) {
+public record EventsRequestDTO(String title, String description, Long date, String state, Boolean remote, String eventUrl, String imgUrl) {
 
 }
     
--- src/main/java/com/spring/Api/services/EventsService.java
+++ src/main/java/com/spring/Api/services/EventsService.java
@@ -5,7 +5,6 @@ package com.spring.Api.services;
 import java.sql.Date;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.spring.Api.domain.event.EventsRequestDTO;
 import com.spring.Api.domain.event.Events;
@@ -14,22 +13,13 @@ import com.spring.Api.domain.event.Events;
 public class EventsService {
     public Events createEvent(EventsRequestDTO data) {
 
-       String imgURL = null;
-
-       if (data.image() != null) {
-        imgURL = this.uploadImg((data.image()));
-       } 
-
        Events newEvent = new Events();
        newEvent.setTitle(data.title());
        newEvent.setDescription(data.description());
-       newEvent.setEventUrl(data.eventURL());
+       newEvent.setEventUrl(data.eventUrl());
        newEvent.setDate(new Date(data.date()));
-       newEvent.setImgUrl(imgURL);
+       newEvent.setImgUrl(data.imgUrl());
 
        return newEvent;
-    }
-    private String uploadImg(MultipartFile multipartFile){
-        return "";
     }
 }
